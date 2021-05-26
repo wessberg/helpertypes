76d040849e3099fe2e94bd5541b0819b5d1fcbca
wessberg <frederikwessberg@gmail.com>
2021-05-26 13:07:03 +0200

    first commit

---
+++ sandhog.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	...require("@wessberg/ts-config/sandhog.config"),
+	logo: {
+		url: "https://raw.githubusercontent.com/wessberg/cjstoesm/master/documentation/asset/logo.png",
+		height: 150
+	}
+};
